3cdce009d71c0b51d9a963f26118bd6a22943663
NFalconi <nelson_falconi@hotmail.com>
2023-03-23 20:48:41 -0300

    inserção de dados no component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <PrimeiroComponente /> <!--<chamada do componente-->
-  <!--<h1>Ola Mundo!</h1>-->
+  <div>
+    <PrimeiroComponente /> <!--<chamada do componente-->
+    <!--<h1>Ola Mundo!</h1>-->
+    <p>O dado do app é {{ testando }}</p>
+  </div>
 </template>
 
 <script>
@@ -10,6 +13,11 @@
     name: 'App',
     components:{
       PrimeiroComponente
+    },
+    data(){ //também é possível incluir o data aqui
+      return{
+        testando: "Teste"
+      }
     }
   }
 </script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Olá Vue!</h1>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
 </template>
 
 <script>
     export default{
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data(){ // componente de inclusão de dados no componente
+            return{
+                nome: "Nelson",
+                profissao: "programador"
+            }
+        }
     }
 </script>
