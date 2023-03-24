0cfdbe736bae738267f7463d6bb09eea3e23e12e
NFalconi <nelson_falconi@hotmail.com>
2023-03-23 23:36:02 -0300

    hierarquia de componentes e componentização

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,25 @@
     <PrimeiroComponente /> <!--<chamada do componente-->
     <p>O dado do app é {{ testando }}</p>
     <LifeCycle />
+    <Pessoa />
+    <Form />
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue';
   import LifeCycle from './components/LifeCycle.vue';
+  import Pessoa from './components/Pessoa.vue';
+  import Form from './components/form.vue';
+
 
   export default{
     name: 'App',
     components:{
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa,
+      Form
     },
     data(){ //também é possível incluir o data aqui
       return{
